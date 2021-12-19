6a1d4b77bc23740445d587b09165c3a20467cd1d
AmeerAhmed <ameer.kanaaneh@gmail.com>
2021-12-19 11:54:48 +0200

    add functionality to the project

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,35 @@
-let popup = 
+let popup = document.querySelector(".popup");
+let popupCloseBtn = document.querySelector(".popup__close");
+let profileEditBtn = document.querySelector(".profile__edit-button");
+let inputs = document.querySelectorAll(".popup__input");
+let popupForm = document.querySelector(".popup__container");
+let profileName = document.querySelector(".profile__name");
+let profileInterest = document.querySelector(".profile__interest");
+
+profileEditBtn.addEventListener("click", openPopup);
+
+popupCloseBtn.addEventListener("click", closePopup);
+
+function closePopup() {
+  popup.classList.remove("popup_opened");
+}
+
+function openPopup() {
+  popup.classList.add("popup_opened");
+}
+
+function handleProfileFormSubmit(evt) {
+  evt.preventDefault();
+  let nameInput = inputs[0];
+  let jobInput = inputs[1];
+
+  let NameInputVal = nameInput.value;
+  let jobInputVal = jobInput.value;
+
+  profileName.textContent = NameInputVal;
+
+  profileInterest.textContent = jobInputVal;
+
+  closePopup();
+}
+popupForm.addEventListener("submit", handleProfileFormSubmit);
