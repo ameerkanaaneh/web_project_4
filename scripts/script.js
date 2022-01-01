a0626b33f9704354aee1249963d37f466ccf899d
AmeerAhmed <ameer.kanaaneh@gmail.com>
2022-01-01 21:48:10 +0200

    feat/add elements with javascript

--- scripts/script.js
+++ scripts/script.js
@@ -6,6 +6,55 @@ const popupForm = document.querySelector(".popup__form");
 const profileName = document.querySelector(".profile__name");
 const profileInterest = document.querySelector(".profile__interest");
 
+const initialCards = [
+  {
+    name: "Yosemite Valley",
+    link: "https://code.s3.yandex.net/web-code/yosemite.jpg",
+  },
+  {
+    name: "Lake Louise",
+    link: "https://code.s3.yandex.net/web-code/lake-louise.jpg",
+  },
+  {
+    name: "Bald Mountains",
+    link: "https://code.s3.yandex.net/web-code/bald-mountains.jpg",
+  },
+  {
+    name: "Latemar",
+    link: "https://code.s3.yandex.net/web-code/latemar.jpg",
+  },
+  {
+    name: "Vanoise National Park",
+    link: "https://code.s3.yandex.net/web-code/vanoise.jpg",
+  },
+  {
+    name: "Lago di Braies",
+    link: "https://code.s3.yandex.net/web-code/lago.jpg",
+  },
+];
+const elementsSection = document.querySelector(".elements");
+
+console.log(elementsSection);
+
+initialCards.forEach((card) => {
+  appendElement(card.link, card.name);
+});
+
+function appendElement(url, name) {
+  const elementTemplate = document.querySelector("#element").content;
+  const element = elementTemplate.querySelector(".element").cloneNode(true);
+  const imageElement = element.querySelector(".element__image");
+  const nameElement = element
+    .querySelector(".element__box")
+    .querySelector(".element__name");
+
+  imageElement.src = url;
+  nameElement.textContent = name;
+  imageElement.alt = name + " city";
+
+  elementsSection.append(element);
+}
+
 function closePopup() {
   popup.classList.remove("popup_opened");
 }
