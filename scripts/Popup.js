197188a236c05d0f7c902c2de87925fb2b260abb
ameer ahmad <ameer.kanaaneh@gmail.com>
2022-02-19 18:41:31 +0200

    refactor: add popup class

---
+++ scripts/Popup.js
@@ -0,0 +1,33 @@
+export default class Popup {
+  constructor(popupSelector) {
+    this._popup = document.querySelector(popupSelector);
+  }
+
+  _handleEscClose(evt) {
+    if (evt.key === "Escape") {
+      this.closePopup(this._popup);
+    }
+  }
+
+  closePopup() {
+    this._popup.classList.remove("popup_opened");
+    document.removeEventListener("keydown", this._handleEscClose);
+  }
+
+  openPopup() {
+    this._popup.classList.add("popup_opened");
+    document.addEventListener("keydown", this._handleEscClose);
+  }
+
+  _addCloseOnClickEvent() {
+    this._closeBtn = this._popup.querySelector(".popup__close");
+    this._closeBtn.addEventListener("click", (evt) => {
+      evt.preventDefault();
+      this.closePopup();
+    });
+  }
+
+  setEventListeners() {
+    this._addCloseOnClickEvent();
+  }
+}
