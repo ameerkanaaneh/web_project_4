c2fc7638c1ad78c0243e0647b8429f9e51f7ca5a
ameer ahmad <ameer.kanaaneh@gmail.com>
2022-02-19 19:09:43 +0200

    refactor: add popupWithImage class

---
+++ scripts/PopupWithImage.js
@@ -0,0 +1,20 @@
+import Popup from "./Popup";
+
+export default class PopupWithImage extends Popup {
+  constructor(popupSelector) {
+    super(popupSelector);
+  }
+
+  openPopup(src, name) {
+    super.openPopup();
+    this._addImageAndCaption(srx, name);
+  }
+
+  _addImageAndCaption(src, name) {
+    this._popupImageElement = document.querySelector(".popup__card-image");
+    this._popupNameElement = document.querySelector(".popup__name");
+    this._popupImageElement.src = src;
+    this._popupImageElement.alt = name;
+    this._popupNameElement = name;
+  }
+}
